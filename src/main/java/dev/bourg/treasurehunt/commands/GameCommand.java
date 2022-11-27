0907b5b52bb9f42271f173a6ee1325b98f9c3403
bourg <dj.ka@outlook.de>
2022-11-27 16:25:48 +0100

    Added multiple args & cancel subcommand

--- src/main/java/dev/bourg/treasurehunt/commands/GameCommand.java
+++ src/main/java/dev/bourg/treasurehunt/commands/GameCommand.java
@@ -2,6 +2,7 @@ package dev.bourg.treasurehunt.commands;
 
 import dev.bourg.treasurehunt.game.GameManager;
 import dev.bourg.treasurehunt.game.GameState;
+import org.bukkit.GameMode;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandException;
 import org.bukkit.command.CommandExecutor;
@@ -23,7 +24,15 @@ public class GameCommand implements CommandExecutor {
             sender.sendMessage("no");
             return false;
         }
-        gameManager.setGameState(GameState.STARTING);
+        Player player = (Player) sender;
+        if(player.getGameMode() != GameMode.SURVIVAL) return false;
+        if (args.length == 1) {
+            if(args[0].equalsIgnoreCase("start")){
+                this.gameManager.setGameState(GameState.STARTING);
+            }else if(args[0].equalsIgnoreCase("cancel")){
+                this.gameManager.setGameState(GameState.LOBBY);
+            }
+        }
         return false;
     }
 }
