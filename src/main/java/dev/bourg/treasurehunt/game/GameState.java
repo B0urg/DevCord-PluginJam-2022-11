33b585eda59956ccb42af03c581c01be0bf8c6cc
bourg <dj.ka@outlook.de>
2022-11-27 16:07:50 +0100

    Added some additional GameStates

--- src/main/java/dev/bourg/treasurehunt/game/GameState.java
+++ src/main/java/dev/bourg/treasurehunt/game/GameState.java
@@ -2,6 +2,6 @@ package dev.bourg.treasurehunt.game;
 
 public enum GameState {
 
-    LOBBY, STARTING, ACTIVE, FINAL, WON, RESTARTING;
+    LOBBY, STARTING, ACTIVE, FINAL, WON, RESTARTING, QUEST1, QUEST2, SEARCH, DISCOVERING;
 
 }
