e75d4cccedb019dfbce605a0d845ee0ba532bfea
bourg <dj.ka@outlook.de>
2022-11-27 16:06:42 +0100

    Added ItemBuilder.java for some inventorys

---
+++ src/main/java/dev/bourg/treasurehunt/utils/ItemBuilder.java
@@ -0,0 +1,47 @@
+package dev.bourg.treasurehunt.utils;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.Arrays;
+
+public class ItemBuilder {
+    private ItemMeta itemMeta;
+    private ItemStack itemStack;
+    public ItemBuilder(Material mat){
+        itemStack = new ItemStack(mat);
+        itemMeta = itemStack.getItemMeta();
+    }
+    public ItemBuilder setDisplayname(String s){
+        itemMeta.displayName(Component.text(s));
+        return this;
+    }
+    public ItemBuilder setLore(TextComponent... s){
+        itemMeta.lore(Arrays.asList(s));
+        return this;
+    }
+    public ItemBuilder setUnbreakable(boolean s){
+        itemMeta.setUnbreakable(s);
+        return this;
+    }
+    public ItemBuilder addItemFlags(ItemFlag... s){
+        itemMeta.addItemFlags(s);
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "ItemBuilder{" +
+                "itemMeta=" + itemMeta +
+                ", itemStack=" + itemStack +
+                '}';
+    }
+    public ItemStack build(){
+        itemStack.setItemMeta(itemMeta);
+        return itemStack;
+    }
+}
